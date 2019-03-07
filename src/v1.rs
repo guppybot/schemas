b4e3cb57ae891c89098a3742893a79a69ea1b5d7
Peter Jin <peterhaijin@gmail.com>
2019-03-07 13:19:39 -0800

    Remove unused local config.

--- src/v1.rs
+++ src/v1.rs
@@ -261,39 +261,6 @@ pub struct MachineConfigV0 {
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub enum UserDomainV0 {
-  GuppybotOrg,
-  GithubCom,
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct UserHandleV0 {
-  pub username: String,
-  pub domain: UserDomainV0,
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub enum CiEventPolicyV0 {
-  Nobody,
-  AllowedUsers,
-  EverybodyExceptCiChanges,
-  Everybody,
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct CiRepoV0 {
-  pub remote_url: String,
-  pub commit_policy: CiEventPolicyV0,
-  pub pr_policy: CiEventPolicyV0,
-  pub allowed_users: Vec<UserHandleV0>,
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct CiConfigV0 {
-  pub repos: Vec<CiRepoV0>,
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
 pub enum Bot2RegistryV0 {
   _NewCiRun(Option<_NewCiRunV0>),
   _StartCiTask{
